b6c1655c43a1b377d1ac4646585fd70871a38ed2
darinadari13 <92116950+darinadari13@users.noreply.github.com>
2023-03-06 15:05:13 +0000

    Deploying to gh-pages from @ darinadari13/goit-react-hw-08-phonebook@665abb239e145b0dfe9f5d252240bd1275f9ff6a 🚀

---
+++ static/js/936.601679f7.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[936],{4936:function(e,a,t){t.r(a),t.d(a,{default:function(){return o}});t(2791);var r="Home_title__L087v",c="Home_paragraph__wEht8",n=t(184);function o(){return(0,n.jsxs)("div",{children:[(0,n.jsx)("h1",{className:r,children:"Welcome to PhoneBook"}),(0,n.jsx)("p",{className:c,children:"Search, add and manage your contacts easily with our app."})]})}}}]);
+//# sourceMappingURL=936.601679f7.chunk.js.map
